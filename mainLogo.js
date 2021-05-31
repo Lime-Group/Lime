48da4ea54911c7290caa69fa7c758cff5ab261eb
semenoff666 <semenoff1352@gmail.com>
2021-05-31 21:37:06 +0300

    .

--- mainLogo.js
+++ mainLogo.js
@@ -10,6 +10,19 @@ function AddAcIcon(fileName){
   navAcIcon.style.backgroundImage = "url('sprites/" + fileName + "')";
 }
 ////////////////////////////////////////////////////////////////////////////////
-function AddNewPost(f){
-  alert("Вы ввели: " + f.AddNewPostName.value);
+function AddNPOpen(){
+  const main = document.querySelector('.main');
+  main.insertAdjacentHTML(
+    'beforeend',
+    '<div class="AddNPBack"></div>'
+ );
+ const AddNPBack = document.querySelector('.AddNPBack');
+ AddNPBack.insertAdjacentHTML(
+   'beforeend',
+   '<div class="AddNPPlace"><input class="AddNPImage" type="file" accept="image/jpeg,image/png"><input class="AddNPText" placeholder="Текст записи"><div class="AddNPClose" onclick="AddNPClose()"></div><div class="UploadPost" onclick="AddNPClose()"></div></div>'
+);
+}
+function AddNPClose() {
+  const AddNPBack = document.querySelector('.AddNPBack');
+  AddNPBack.remove();
 }
