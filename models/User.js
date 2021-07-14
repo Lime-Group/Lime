1cf686a30d16f374e21486fea854e12ccfda7a35
c12games <57368063+c12games@users.noreply.github.com>
2021-07-14 18:33:07 +0300

    выавыавыавы

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const {Schema, model, Types} = require('mongoose');
 const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    personal_data: {type: Types.ObjectId, },
+    personal_data: {type: Types.ObjectId,},
 });
 
 module.exports = model('User', schema);
