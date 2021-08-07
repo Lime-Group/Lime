efd7c1eb7befdee4c370fb8eecfef9eee3bc0ad9
c12games <57368063+c12games@users.noreply.github.com>
2021-08-07 20:57:50 +0300

    юзера подредачил

--- models/User.js
+++ models/User.js
@@ -6,10 +6,14 @@ const schema = new Schema({
     name: {type: String, required: true},
     surname: {type: String, required: true},
     role: {type: String, required: true, default: "User"},
+    avatar: {type: String, default: ""},
     posts: {type: Types.ObjectId,},
     videos: {type: Types.ObjectId,},
     following: {type: Array, default: []},
     friends: {type: Array, default: []},
-});
+    city : {type: String, max: 50}
+},
+{timestamps: true},
+);
 
 module.exports = model('User', schema);
