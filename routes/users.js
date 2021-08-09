e90a6b3cf64a0c7d71fdf72d3ff993cc696cc236
c12games <57368063+c12games@users.noreply.github.com>
2021-08-09 20:49:37 +0300

    письки

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 const User = require('../models/User');
 const router = require("express").Router();
+const bcrypt = require('bcrypt')
 
 // update user
 router.put("/:id", async(req, res) => {
-    if(req.body.userId === res.params.id || req.user.role == "Admin"){} else{
+    if(req.body.userId === res.params.id || req.user.role == "Admin"){
+        if(req.body.password){
+            try {
+                req.body.password = bcrypt.hash(req.body.password, 12);
+            } catch (error) {
+                return res.json(error);
+            }
+            req.body.password = bcrypt.hash(req.body.password, 12);
+        }
+    } else{
         return res.status(403).json({ message : "Вы можете обновить только свой аккаунт"})
     }
 });
